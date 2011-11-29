0d95491670049fa1835b53b1b2395291d1e6fb1e
Terrance Ng <bluavenger@gmail.com>
2011-11-29 12:12:29 -0800

    named_scope deprecated in Rails 3, fixed

--- app/models/log.rb
+++ app/models/log.rb
@@ -14,11 +14,11 @@ class Log < ActiveRecord::Base
   attr_accessible :key, :value, :additional_data
   
   default_scope :order => "created_at desc", :limit => 25
-  named_scope :shows_updated, :conditions => { :key => "shows_updated" }
-  named_scope :episodes_updated, :conditions => { :key => "episodes_updated" }
-  named_scope :banners_updated, :conditions => { :key => "banners_updated" }
-  named_scope :show_trends_updated, :conditions => { :key => "show_trends_updated" }
-  named_scope :show_requested, :conditions => { :key => "show_requested" }
+  scope :shows_updated, :conditions => { :key => "shows_updated" }
+  scope :episodes_updated, :conditions => { :key => "episodes_updated" }
+  scope :banners_updated, :conditions => { :key => "banners_updated" }
+  scope :show_trends_updated, :conditions => { :key => "show_trends_updated" }
+  scope :show_requested, :conditions => { :key => "show_requested" }
   
   def failed?
     value.blank?
