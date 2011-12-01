25c0843b0d4f842fc68a5ac95517500b1b5b5688
Terrance Ng <bluavenger@gmail.com>
2011-12-01 02:37:42 -0800

    ActiveAdmin for FAQs.

--- app/models/faq.rb
+++ app/models/faq.rb
@@ -1,5 +1,19 @@
 class Faq < ActiveRecord::Base
-  scope :important, where ("important = true")
+  after_initialize :default_values
+  
+  scope :important, :conditions => "important = true"
+  scope :unimportant, :conditions => "not important = true"
+  scope :recently_asked, where("created_at > ?", 5.days.ago)
   
   attr_accessible :question, :answer, :position, :important
+  
+  def important?
+    self.important
+  end
+  
+  private
+  
+  def default_values
+    self.position ||= 0
+  end
 end
