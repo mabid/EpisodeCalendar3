2a9f7fc61ac2b39164521af14ff8cafadad5cdfb
Terrance Ng <bluavenger@gmail.com>
2011-12-02 12:14:07 -0800

    Reordered important FAQ questions, added highlight color.

--- app/models/faq.rb
+++ app/models/faq.rb
@@ -1,6 +1,7 @@
 class Faq < ActiveRecord::Base
   
   default_scope order("position asc, created_at asc")
+  scope :important, where(:important => true).order("position ASC")
   
   attr_accessible :question, :answer, :position, :important
   
