0d76a517d0fe5139d05fe6b76dd43d7439eab07d
frexuz <frexuz@frexuz.com>
2011-12-10 20:50:51 +0100

    More FAQ styling etc..

--- app/admin/faqs.rb
+++ app/admin/faqs.rb
@@ -8,7 +8,9 @@ ActiveAdmin.register Faq do
     column :question, :sortable => false do |faq|
       link_to faq.question, admin_faq_path(faq)
     end
-    column :answer
+    column :answer do |faq|
+      truncate(faq.answer, :length => 50)
+    end
     column :position
     column :important, :sortable => false do |faq|
       if faq.important?
@@ -22,4 +24,20 @@ ActiveAdmin.register Faq do
       raw "#{link_to "Edit", edit_admin_faq_path(faq), :class => :member_link} #{link_to "Delete", admin_faq_path(faq), :class => :member_link, :method => :delete, :confirm => "Delete question '#{faq.question}'?"}" + icon_tag("drag_handle")
     end
   end
+
+  form do |f|
+    size = Faq.all.size
+    f.inputs do
+      f.input :question
+      f.input :answer
+      if f.object.new_record?
+        f.input :position, :input_html => { :value => size + 1 }
+      else
+        f.input :position
+      end
+      f.input :important
+      f.buttons
+    end
+  end
+    
 end
