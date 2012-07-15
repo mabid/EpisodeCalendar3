75b908f6762fa8e19b9857c5649dc6e8f7d7cd8e
frexuz <frexuz@frexuz.com>
2012-07-15 19:07:42 +0200

    Created a db:seed to import the top 100 shows

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,14 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+#Top 100 most popular shows from the production server (2012-06-15)
+api_show_ids = [80379, 75760, 79349, 121361, 153021, 73255, 82066, 95011, 82283, 81189, 83610, 94571, 80348, 80349, 95491, 248682, 73762, 75978, 73244, 78901, 72227, 80547, 82459, 248835, 75897, 74845, 176941, 75682, 248646, 79488, 108611, 80337, 164091, 84947, 129261, 73800, 247897, 73871, 71663, 146711, 247808, 248742, 205281, 80270, 83462, 250487, 79334, 124051, 248837, 74543, 83602, 73739, 84912, 248736, 164301, 73141, 84676, 75710, 248741, 248935, 94991, 82696, 210841, 72108, 75805, 78804, 164541, 79335, 82339, 76354, 79501, 83123, 163531, 248884, 74608, 82716, 95451, 72546, 248735, 104281, 83237, 134241, 167571, 80542, 72218, 161511, 164021, 248880, 72158, 79216, 75340, 92411, 158661, 95441, 248868, 210171, 79168, 73388, 79773, 110381]
+api_show_ids.each do |api_show_id|
+	UpdateQueue.find_or_create_by_api_id_and_update_type(:api_id => api_show_id, :update_type => "show")
+	UpdateQueue.find_or_create_by_api_id_and_update_type(:api_id => api_show_id, :update_type => "all_episodes")
+end
+
+puts "Queued shows for import. Run these commands to complete the import:"
+puts "1. rake db:update_shows"
+puts "2. rake db:get_show_episodes"
