d5ccac7cc89c6d9bdfb315f7d2faf78df9a710ba
frexuz <frexuz@frexuz.com>
2012-11-10 19:47:50 +0100

    Mistake!

--- app/sweepers/following_sweeper.rb
+++ app/sweepers/following_sweeper.rb
@@ -5,7 +5,6 @@ class FollowingSweeper < ActionController::Caching::Sweeper
     if following.cached_show.followers >= 10
       expire_fragment("footer_recently_favorited") 
       expire_fragment("footer_tag_cloud")
-      Rails.cache.expire([following, :show])
     end
   end
   
