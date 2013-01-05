55a80b6defa55122f7a4bd0296cbb9656f3e32d3
frexuz <frexuz@frexuz.com>
2013-01-05 22:31:22 +0100

    Made some cleanup on the subscription page/form :)

--- app/assets/javascripts/subscriptions.js
+++ app/assets/javascripts/subscriptions.js
@@ -4,29 +4,13 @@ $(function(){
 
     $(window).load(check_form_filled());
     $('.radio_button').click( function() {
-        var total_months = parseInt(this.value.split('-')[0]) + 1;
-        var monthNames = [ "January", "February", "March", "April", "May", "June",
-            "July", "August", "September", "October", "November", "December" ];
-        var today = new Date;
-        var expirtion_date = new Date(today.getFullYear(), today.getMonth() + total_months, today.getDay());
-        var plan_info = monthNames[expirtion_date.getMonth()] + ' ' + expirtion_date.getDate() + ' ' + expirtion_date.getFullYear();
-        $('.plan_info').html(plan_info).slideDown('slow');
+        $("#plan_info span").hide();
+        $("#expiration_date_" + $(this).closest("td").index()).show();
         check_form_filled();
     });
 
     $('input[type=text]').bind('click blur', function() {
         check_form_filled();
-    });
-
-    $("table").delegate('td,th','mouseover mouseleave', function(e) {
-        if (e.type == 'mouseover') {
-            var index = $(this).index();
-            $("td."+index).addClass("hover");
-        }
-        else {
-            var index = $(this).index();
-            $("td."+index).removeClass("hover");
-        }
     });
 
     function check_form_filled(){
